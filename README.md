4c9337785b2bbc992b8daf2e60ee6c592c6d5c16
-- <-->
2020-02-19 11:31:04 -0500

    First crack at fun with strict checks

--- README.md
+++ README.md
@@ -25,3 +25,16 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+
+
+## Helpful Links
+
+### Type Safety - TS Lint
+- https://mariusschulz.com/blog/strict-property-initialization-in-typescript 
+- https://stackoverflow.com/questions/42793701/is-there-a-way-to-enforce-method-return-types-on-typescript-classes-via-a-tslint
+- https://palantir.github.io/tslint/rules/typedef/
+
+- https://dev.to/briwa/how-strict-is-typescript-s-strict-mode-311a
+- https://medium.com/lacolaco-blog/guide-for-type-safe-angular-6e9562499d93 
+
+- https://stackoverflow.com/questions/32227277/typescript-typed-function-argument-accepts-any
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,20 @@
 import { Component } from '@angular/core';
 
+class Foo {
+  bar: string | undefined;
+}
+
+class Article {
+  id: number | undefined;
+  meta: string | undefined;
+}
+
+enum Vehicle {
+  Car = 1,
+  Bike = 2,
+  Truck = 3
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +22,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'type-safe-testing';
+  foo = new Foo();
+
+  constructor() {
+    const anyValue = 'dsfsf';
+    const articles: Article[] = [];
+
+    this.testNoImplicitAny(anyValue);
+    this.getArticlesByMetaById(articles, 3);
+    this.testEnum('Car');
+    this.testEnum(1);
+  }
+
+  private testNoImplicitAny(value: string): void {
+
+  }
+
+  private getArticlesByMetaById(articles: Article[], id: number): string | undefined {
+
+    const article: Article | undefined = articles.find(a => a.id === id);
+
+    if (typeof article === 'undefined') {
+      // throw new Error(`Could not find article with id : ${id}`);
+      console.error(`Could not find article with id : ${id}`);
+      return undefined;
+    }
+
+    return article.meta;
+  }
+
+  private testEnum(value: string | number): void {
+
+    if (Object.values(Vehicle).includes(value)) {
+      alert(`Is Enum === true: val {${value}}`);
+    }
+  }
 }
